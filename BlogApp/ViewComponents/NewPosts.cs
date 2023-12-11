a888535d02fb211a163471f9915833703163f843
GokhanKus <gkus1998@hotmail.com>
2023-12-11 13:05:00 +0300

    ViewComponent posts

---
+++ BlogApp/ViewComponents/NewPosts.cs
@@ -0,0 +1,25 @@
+﻿using BUSINESS.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.ViewComponents
+{
+	public class NewPosts:ViewComponent
+	{
+		private readonly IPostRepository _postRepository;
+		public NewPosts(IPostRepository postRepository)
+        {
+			_postRepository = postRepository;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var model = await _postRepository
+				.Posts
+				.OrderByDescending(d => d.CreatedTime)
+				.Take(5)
+				.ToListAsync();
+
+			return View(model);
+		}
+    }
+}
